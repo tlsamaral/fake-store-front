c857a37569b9531c344f91ef2c0343084c09683d
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 01:06:26 -0300

    chore(api): setup Axios instance for API requests
    - Added src/lib/axios.ts for a pre-configured Axios instance.
    - Simplifies API requests with base URL and interceptors.
    - Improves code consistency and error handling.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// Poderia utilizar uma env variable para isso, mas como é um caso simples, deixei a url por aqui mesmo.
 export const api = axios.create({
 	baseURL: 'https://fakestoreapi.com',
 })
