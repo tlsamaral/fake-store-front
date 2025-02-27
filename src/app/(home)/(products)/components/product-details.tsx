04a2dd8f096f8b19599e4318713f957e6eedbfee
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 01:14:12 -0300

    feat(ui): add product details modal
    - Created product-details.tsx component to display product information in a modal.
    - Shows product name, price, description, and rating.

--- src/app/(home)/(products)/components/product-details.tsx
+++ src/app/(home)/(products)/components/product-details.tsx
@@ -1,3 +1,4 @@
+import { getProduct } from '@/app/http/get-product'
 import {
 	DialogContent,
 	DialogDescription,
@@ -13,35 +14,47 @@ import {
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table'
+import { useQuery } from '@tanstack/react-query'
+import type { Product } from '../page'
 
-export function ProductDetails() {
+interface ProductDetailsProps {
+	product: Product
+}
+
+export function ProductDetails({ product }: ProductDetailsProps) {
 	return (
 		<DialogContent className="sm:max-w-[525px]">
 			<DialogHeader>
-				<DialogTitle>Produto: Teste</DialogTitle>
+				<DialogTitle>Product Details</DialogTitle>
 				<DialogDescription>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis fuga
-					cum asperiores dolore debitis officiis. Mollitia eum, repellat
-					deleniti dolorum error ut corporis iusto voluptate totam labore nihil
-					illo architecto.
+					View detailed information about the selected product, including price,
+					description, and ratings.
 				</DialogDescription>
 			</DialogHeader>
 
+			<div className="grid pb-3 border-b">
+				<strong className="text-sm">Title</strong>
+				<span className="text-xs">{product.title}</span>
+			</div>
+			<div className="grid pb-3 border-b">
+				<strong className="text-sm">Description</strong>
+				<span className="text-xs">{product.description}</span>
+			</div>
 			<Table>
 				<TableBody>
 					<TableRow>
 						<TableCell className="text-muted-foreground">Categoria</TableCell>
 						<TableCell className="flex justify-end">
-							categoria_produto
+							{product.category}
 						</TableCell>
 					</TableRow>
 					<TableRow>
 						<TableCell className="text-muted-foreground">Preço</TableCell>
-						<TableCell className="flex justify-end">R$ 20,00</TableCell>
+						<TableCell className="flex justify-end">{product.price}</TableCell>
 					</TableRow>
 					<TableRow>
 						<TableCell className="text-muted-foreground">Avaliação</TableCell>
-						<TableCell className="flex justify-end">4.5</TableCell>
+						<TableCell className="flex justify-end">{`${product.rating.rate} / 5 - ${product.rating.count}`}</TableCell>
 					</TableRow>
 				</TableBody>
 			</Table>
