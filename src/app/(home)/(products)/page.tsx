b5e75acedf9db3dfd0deca2915e703e342625272
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 01:09:03 -0300

    feat(products): implement sorting for product list
    - Added sorting logic to display products in a structured order.
    - Ensured higher-rated products (rating 4.5+) appear at the top.

--- src/app/(home)/(products)/page.tsx
+++ src/app/(home)/(products)/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+import { fetchProducts } from '@/app/http/fetch-products'
 import { ProductTable } from './components/products-table'
 
 export interface Product {
@@ -15,55 +14,21 @@ export interface Product {
 	}
 }
 
-const products: Product[] = [
-	{
-		id: 1,
-		title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-		price: '$109.95',
-		category: 'men clothing',
-		description:
-			'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) and your everyday',
-		image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-		rating: {
-			rate: 3.9,
-			count: 120,
-		},
-	},
-	{
-		id: 2,
-		title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-		price: '$109.95',
-		category: 'men clothing',
-		description:
-			'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) and your everyday',
-		image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-		rating: {
-			rate: 3.9,
-			count: 120,
-		},
-	},
-	{
-		id: 3,
-		title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-		price: '$109.95',
-		category: 'men clothing',
-		description:
-			'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) and your everyday',
-		image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-		rating: {
-			rate: 3.9,
-			count: 120,
-		},
-	},
-]
+export default async function Home() {
+	const products = await fetchProducts()
+	const sortedProducts = products.sort((a: Product, b: Product) => {
+		if (b.rating.rate >= 4.5 && a.rating.rate < 4.5) return 1
+		if (a.rating.rate >= 4.5 && b.rating.rate < 4.5) return -1
 
-export default function Home() {
+		return b.rating.rate - a.rating.rate
+	})
+
 	return (
 		<div className="flex flex-col gap-4">
 			<h1 className="text-3xl font-bold tracking-tight">Products</h1>
 
 			<div className="space-y-2.5">
-				<ProductTable products={products} />
+				<ProductTable products={sortedProducts} />
 			</div>
 		</div>
 	)
