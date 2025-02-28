3329fbea03cf7096ea0f20d64ac1d049a18e9e3a
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 22:23:16 -0300

    feat(api): create separate function for fetching categories

--- src/app/http/fetch-categories.ts
+++ src/app/http/fetch-categories.ts
@@ -1,5 +1,4 @@
 import { api } from '@/lib/axios'
-import type { Product } from '../(home)/(products)/page'
 
 export async function fetchCategories() {
 	const response = await api.get<string[]>('/products/categories')
