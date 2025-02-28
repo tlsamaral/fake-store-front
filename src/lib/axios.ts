428f3bc9c267beb70d76c4bc1bc73e86cd12989d
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 22:25:32 -0300

    chore(api): add delay in Axios requests for loading states
    - Added artificial delay in xios.ts to improve loading state handling.
    - Helps simulate real-world API response times for better UX.
    - Ensures smoother transitions in UI components that depend on API calls.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,3 +4,11 @@ import axios from 'axios'
 export const api = axios.create({
 	baseURL: 'https://fakestoreapi.com',
 })
+
+api.interceptors.request.use(async (config) => {
+	await new Promise((resolve) =>
+		setTimeout(resolve, Math.floor(Math.random() * 3000)),
+	)
+
+	return config
+})
