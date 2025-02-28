86bb1cc7a92703886b33340e80b2cf785a62d6c4
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-02-27 22:24:04 -0300

    feat(api): create separate function for fetching a single product

--- src/app/http/get-product.ts
+++ src/app/http/get-product.ts
@@ -1,5 +1,17 @@
 import { api } from '@/lib/axios'
-import type { Product } from '../(home)/(products)/page'
+
+export interface Product {
+	id: number
+	title: string
+	price: number
+	category: string
+	description: string
+	image: string
+	rating: {
+		rate: number
+		count: number
+	}
+}
 
 export async function getProduct(id: number) {
 	const response = await api.get<Product>(`/products/${id}`)
