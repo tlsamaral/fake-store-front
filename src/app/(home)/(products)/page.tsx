1512e743569b2e26ae980cd7812677dcda10dc3c
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-02 18:55:00 -0300

    refactor(products-page): replace Next.js SSR with React Query
    - Removed server-side data fetching to improve client-side performance.
    - Integrated React Query for better caching and state management.
    - Enhances responsiveness and reduces server load.

--- src/app/(home)/(products)/page.tsx
+++ src/app/(home)/(products)/page.tsx
@@ -1,4 +1,5 @@
 import { fetchProducts } from '@/app/http/fetch-products'
+import { useQuery } from '@tanstack/react-query'
 import { ProductTable } from './components/products-table'
 
 export interface Product {
@@ -14,21 +15,22 @@ export interface Product {
 	}
 }
 
-export default async function Home() {
-	const products = await fetchProducts()
-	const sortedProducts = products.sort((a: Product, b: Product) => {
-		if (b.rating.rate >= 4.5 && a.rating.rate < 4.5) return 1
-		if (a.rating.rate >= 4.5 && b.rating.rate < 4.5) return -1
+// Como é um teste, eu quis ter uma UI&UX um pouco mais interessante, então optei por fazer a busca de produtos com o React query e não usar o SSR do Next.js.
+export default function Home() {
+	// const products = await fetchProducts()
+	// const sortedProducts = products.sort((a: Product, b: Product) => {
+	// 	if (b.rating.rate >= 4.5 && a.rating.rate < 4.5) return 1
+	// 	if (a.rating.rate >= 4.5 && b.rating.rate < 4.5) return -1
 
-		return b.rating.rate - a.rating.rate
-	})
+	// 	return b.rating.rate - a.rating.rate
+	// })
 
 	return (
-		<div className="flex flex-col gap-4 px-4 md:px-6 ">
+		<div className="flex flex-col gap-4 max-w-7xl mx-auto">
 			<h1 className="text-3xl font-bold tracking-tight">Products</h1>
 
 			<div className="space-y-2.5">
-				<ProductTable products={sortedProducts} />
+				<ProductTable />
 			</div>
 		</div>
 	)
