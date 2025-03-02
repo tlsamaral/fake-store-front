280908d15b473028166bdb3e370c067c67850082
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-03-02 18:50:00 -0300

    refactor(products-layout): remove ThemeProvider to use it in the main layout
    - Removed ThemeProvider from the products layout.
    - Ensures global theme management by moving it to the root layout.
    - Improves consistency and avoids redundant context providers.

--- src/app/(home)/(products)/layout.tsx
+++ src/app/(home)/(products)/layout.tsx
@@ -1,36 +1,25 @@
-import { Header } from '@/components/header'
-import { ThemeProvider } from '@/components/theme-provider'
-import { ReactQueryProvider } from '@/providers/react-query-provider'
-import { Toaster } from 'sonner'
+'use client'
 
-export default function RootLayout({
+import { Header } from '@/components/header'
+
+export default function HomeLayout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
-		<ReactQueryProvider>
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="system"
-				enableSystem
-				disableTransitionOnChange
-			>
-				<Toaster richColors position="top-center" />
-				<Header />
-
-				<main className="max-w-7xl mx-auto mt-4">{children}</main>
-
-				<footer className="mt-auto">
-					<p className="text-center text-sm leading-loose">
-						Created by{' '}
-						<a
-							className="underline underline-offset-2"
-							href="https://tallesamaral.dev"
-						>
-							Talles Amaral
-						</a>
-					</p>
-				</footer>
-			</ThemeProvider>
-		</ReactQueryProvider>
+		<>
+			<Header />
+			<main className="px-4 md:px-6 mx-auto mt-4">{children}</main>
+			<footer className="mt-auto">
+				<p className="text-center text-sm leading-loose">
+					Created by
+					<a
+						className="underline underline-offset-2"
+						href="https://tallesamaral.dev"
+					>
+						Talles Amaral
+					</a>
+				</p>
+			</footer>
+		</>
 	)
 }
